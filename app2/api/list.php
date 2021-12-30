82d98df430cf1ec3616f837c456b0905f3765dc3
MoonLin <mooncactusmoon@gmail.com>
2021-12-30 12:15:56 +0800

    使用ajax來載入各班級的學生資料

--- app2/api/list.php
+++ app2/api/list.php
@@ -3,15 +3,18 @@ include_once "base.php";
 
 $sql="SELECT `classroom` as '班級',count(`id`) as '人數' FROM `students` GROUP BY `classroom`;";
 $rows=$Stu->q($sql);
-// echo json_encode($rows); 讓前端做api要用的
-$list="";
-foreach($rows as $row){
-    $list.="<div class='card'>";
-    $list.="<div class='card-body'>";
-    $list.="<h5 class='card-title'>{$row['班級']}</h5>";
-    $list.="<p class='card-text'>{$row['人數']}</p>";
-    $list.="</div>";
-    $list.="</div>";
-}
-echo $list;
+echo json_encode($rows); //讓前端做api要用的
+
+//後端傳輸
+// $list="";
+// foreach($rows as $row){
+//     $list.="<div class='card'>";
+//     $list.="<div class='card-body'>";
+//     $list.="<h5 class='card-title'>{$row['班級']}</h5>";
+//     $list.="<p class='card-text'>{$row['人數']}</p>";
+//     $list.="</div>";
+//     $list.="</div>";
+// }
+// echo $list;
+//後端傳輸 end
 ?>
